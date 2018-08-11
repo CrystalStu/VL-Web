6130b7f070deae65cb1c75677ca4ea18f8dd22c6
TURX <turuixuan@foxmail.com>
2018-08-11 23:57:21 +0800

    Add IP verification.

--- js/url.js
+++ js/url.js
@@ -1,4 +1,4 @@
 var fqdnHost = window.location.host;
-if(fqdnHost != "vl.cstu.gq" && fqdnHost != "localhost") {
-    window.location.href = "https://vl.cstu.gq";
+if(fqdnHost != "vl.cstu.gq" && fqdnHost != "localhost" && returnCitySN["cip"] != "104.238.185.128") {
+    window.location.href = "https://vl.cstu.gq" + window.location.pathname;
 }
